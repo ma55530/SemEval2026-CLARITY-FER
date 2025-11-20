cc1678f2d0ee68ae52edeed6c8d7fd4625327654
Luka C. <lukacuturilo2709@gmail.com>
2025-11-21 00:26:30 +0300

    dvostruka kvalifikacija i pokusaj koristenja smotea

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from datasets import load_dataset
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import f1_score, confusion_matrix
@@ -6,6 +5,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC, LinearSVC
 from sklearn.naive_bayes import MultinomialNB
 import pandas as pd
+from imblearn.over_sampling import SMOTE
 
 from Interview import Interview, Clarity
 
@@ -14,7 +14,14 @@ dataset = load_dataset("ailsntua/QEvasion")
 trainingData = dataset["train"]
 testingData = dataset["test"]
 
-df = pd.DataFrame(columns=["model","f1 score","CLEAR_REPLY correct","AMBIVALENT correct","CLEAR_NON_REPLY correct"])
+df = pd.DataFrame(columns=[
+    "model",
+    "binary_f1",
+    "3class_f1",
+    "CLEAR_REPLY correct",
+    "AMBIVALENT correct",
+    "CLEAR_NON_REPLY correct"
+])
 
 trainingInterviews = Interview(trainingData)
 testingInterviews = Interview(testingData)
@@ -29,6 +36,12 @@ vectorizer = TfidfVectorizer()
 train_x_vectors = vectorizer.fit_transform(train_x)
 test_x_vectors = vectorizer.transform(test_x)
 
+def to_binary(labels):
+    return [1 if l == Clarity.CLEAR_REPLY else 0 for l in labels]
+
+train_y_bin = to_binary(train_y)
+test_y_bin = to_binary(test_y)
+
 models = {
     "SVC": SVC(class_weight='balanced'),
     "LogisticRegression": LogisticRegression(max_iter=2000, class_weight="balanced"),
@@ -36,15 +49,33 @@ models = {
     "NaiveBayes": MultinomialNB()
 }
 
-labels = [Clarity.CLEAR_REPLY, Clarity.AMBIVALENT, Clarity.CLEAR_NON_REPLY]
+three_class_labels = [Clarity.CLEAR_REPLY, Clarity.AMBIVALENT, Clarity.CLEAR_NON_REPLY]
 
 for model_name, clf in models.items():
-    clf.fit(train_x_vectors, train_y)
-    y_pred = clf.predict(test_x_vectors)
-    score = f1_score(test_y, y_pred, average="macro", labels=labels)
-    cm = confusion_matrix(test_y, y_pred, labels=labels)
+    clf.fit(train_x_vectors, train_y_bin)
+    pred_bin = clf.predict(test_x_vectors)
+    binary_f1 = f1_score(test_y_bin, pred_bin)
+    mask_non_clear = pred_bin == 0
+    train_x_3 = train_x_vectors
+    train_y_3 = train_y
+    smote = SMOTE()
+    train_x_3_balanced, train_y_3_balanced = smote.fit_resample(train_x_3, train_y_3)
+    clf_3 = models[model_name]  
+    clf_3.fit(train_x_3_balanced, train_y_3_balanced)
+    pred_3class = clf_3.predict(test_x_vectors)
+    final_pred = []
+    for b, p3 in zip(pred_bin, pred_3class):
+        if b == 1:
+            final_pred.append(Clarity.CLEAR_REPLY)
+        else:
+            final_pred.append(p3)
+    f1_macro = f1_score(test_y, final_pred, average="macro", labels=three_class_labels)
+    cm = confusion_matrix(test_y, final_pred, labels=three_class_labels)
     df.loc[len(df)] = [
-        model_name, score, cm[0 ,0], cm[1 ,1], cm[2 ,2]
+        model_name,
+        binary_f1,
+        f1_macro,
+        cm[0,0], cm[1,1], cm[2,2]
     ]
 
-df.to_csv("results.csv", index=False)
+df.to_csv("results_double_classifier_smote.csv", index=False)
